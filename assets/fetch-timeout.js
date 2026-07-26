8c82d8483fb3b357b227d2d6f06a457ef0262ec8
Ravikxx <fearlessaviatorclan@gmail.com>
2026-07-26 12:34:42 -0400

    Fix site-wide consistency regressions [skip announcement email] (#80)

---
+++ assets/fetch-timeout.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict'
+
+  var nativeFetch = window.fetch.bind(window)
+
+  window.axionFetch = function (input, options, timeoutMs) {
+    var init = options || {}
+    var upstreamSignal = init.signal
+    var controller = new AbortController()
+    var timeout = Number.isFinite(timeoutMs) ? timeoutMs : 12000
+
+    function forwardAbort() {
+      controller.abort(upstreamSignal && upstreamSignal.reason)
+    }
+
+    if (upstreamSignal) {
+      if (upstreamSignal.aborted) forwardAbort()
+      else upstreamSignal.addEventListener('abort', forwardAbort, { once: true })
+    }
+
+    var timer = setTimeout(function () {
+      controller.abort(new DOMException('Request timed out', 'TimeoutError'))
+    }, timeout)
+
+    return nativeFetch(input, Object.assign({}, init, { signal: controller.signal }))
+      .finally(function () {
+        clearTimeout(timer)
+        if (upstreamSignal) upstreamSignal.removeEventListener('abort', forwardAbort)
+      })
+  }
+})()
